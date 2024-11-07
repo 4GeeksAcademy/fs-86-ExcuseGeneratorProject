36623acb5433249c35c457e292c1a9773834883d
M.V <marco.mov98@gmail.com>
2024-11-07 01:02:37 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,53 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  function sandwichLoco() {
+    const pan = obtenerElTipoDePanAleatorio();
+    const proteina = obtenerProteinaAleatoria();
+    const vegetable = obtenerVegetablesAleatorio();
+    const sauce = obtenerSauceAleatorio();
+    const sandwich = armarSandwich(pan, proteina, vegetable, sauce);
+    return sandwich;
+  }
+
+  function obtenerElTipoDePanAleatorio() {
+    const tiposDePan = new Array("Avena", "Centeno", "Blanco");
+    const posicionDelPanRandom = Math.floor(Math.random() * tiposDePan.length);
+    return tiposDePan[posicionDelPanRandom];
+  }
+
+  function obtenerProteinaAleatoria() {
+    const tiposDeProteina = new Array(
+      "jamón",
+      "pollo terayaqui",
+      "carne con queso"
+    );
+    const posicionDeLaProteinaRandom = Math.floor(
+      Math.random() * tiposDeProteina.length
+    );
+    return tiposDeProteina[posicionDeLaProteinaRandom];
+  }
+
+  function obtenerVegetablesAleatorio() {
+    const tiposDeVegetables = new Array("lechuga", "tomate", "aceitunas");
+    const posicionDeVegetablesRandom = Math.floor(
+      Math.random() * tiposDeVegetables.length
+    );
+    return tiposDeVegetables[posicionDeVegetablesRandom];
+  }
+
+  function obtenerSauceAleatorio() {
+    const tiposDeSauce = new Array("ketchup", "Mahonesa", "Barbacoa");
+    const posicionDeSauceRandom = Math.floor(
+      Math.random() * tiposDeSauce.length
+    );
+    return tiposDeSauce[posicionDeSauceRandom];
+  }
+
+  function armarSandwich(pan, proteina, vegetable, sauce) {
+    return `${pan} ${proteina} ${vegetable} ${sauce}.`;
+  }
+
+  document.getElementById("Dorian").innerHTML = sandwichLoco();
   console.log("Hello Rigo from the console!");
 };
